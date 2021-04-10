722aabf39c8e7a20608398019e4e693e83a3df7e
AlexanderBowers <7319abowers@gmail.com>
2021-04-10 13:29:10 -0700

    adding parse

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
     def get_request
         id = Search.get_id(params[:name])
         puts params[:name]
-        puts 'id is' + id
+        puts 'id is' + JSON.parse(id)
         request = Search.get_tweets(id["data"][0]["id"])
         render json: request
     end 
