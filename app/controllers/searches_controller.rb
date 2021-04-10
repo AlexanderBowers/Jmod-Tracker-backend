d1647caa7c22d221e417e09ef5881e1ca161ba95
AlexanderBowers <7319abowers@gmail.com>
2021-04-10 13:50:37 -0700

    added puts

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@ class SearchesController < ApplicationController
 
     def get_request
         id = Search.get_id(params[:name])
+        puts "id is #{id}"
         request = Search.get_tweets(id["data"][0]["id"])
         render json: request
     end 
